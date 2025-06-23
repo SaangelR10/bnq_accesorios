4bd9e1eb6e8d6fd9ae715387562dd7e17690c32c
SaangelR10 <75223784+SaangelR10@users.noreply.github.com>
2025-06-23 11:11:10 -0500

    front order

---
+++ backend/target/classes/static/tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [
+    './*.html',
+    './js/**/*.js',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        brandy: {
+          50: '#fbf8f1',
+          100: '#f6eede',
+          200: '#eddabb',
+          300: '#e5c89f',
+          400: '#d49e63',
+          500: '#cb8544',
+          600: '#bd7039',
+          700: '#9d5931',
+          800: '#7e482e',
+          900: '#663d28',
+          950: '#371e13',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
